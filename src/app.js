eccd90e69faa70e8a567d07d8c4b96d701793a45
francisco <franc1sc1566@gmail.com>
2020-01-24 21:02:32 -0300

    FIRST

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-require('dotenv').config({
-    path: process.env.NODE_ENV === "test" ? '.env.test' : '.env'
-})
+require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
@@ -12,16 +10,26 @@ class AppController{
         this.app = express()
         this.middlewares()
         this.routes()
+        this.database()
     }
 
     middlewares(){
         this.app.use(express.json())
+        this.app.use(express.urlencoded({extended: false}))
         this.app.use(cors())
         this.app.use(compression())
         this.app.use(helmet())
     }
     routes(){
         this.app.use(require('./routes.js'))
+    }
+    async database(){
+        const { connect } = require('mongoose')
+        await connect('mongodb://localhost/try3',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            
+        })
     }
 }
 
--- src/utils/rateLimit.js
+++ src/utils/rateLimit.js
@@ -2,6 +2,6 @@ const rateLimit = require("express-rate-limit");
 
 exports.createAccountLimiter = rateLimit({
     windowMs: 60 * 60 * 1000,
-    max: 2,
+    max: 2000,
     message: "Too many requests from this IP, please try again after an hour"
 })
